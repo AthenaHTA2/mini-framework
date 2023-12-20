a1dea9958efac494ac3b1a7ce233097d1ffc8d50
AthenaHTA2 <sedmakh2@gmail.com>
2023-12-20 19:19:23 +0000

    array of tasks available

--- components/createInput.jsx
+++ components/createInput.jsx
@@ -1,27 +1,25 @@
   import {Web_pilot} from "../web_pilot/web_pilot.jsx"
-  import { BottomLeft } from "./bottomLeft.jsx"
 
-  export var event
+  //shows number of todos bottom left
   export var increment = 0
+  //The tasks array is a global variable kept in browser's local storage
+  // let ArrayOfTasks = [];
+  var OneTask
 
   
 /** @jsx Web_pilot.createElement */
-
-
-
 export function CreateInput() {
   const appendHere = document.querySelector(".todo-list");
   const displayNum = document.getElementById("todo-count");
 
   
-      const NewTodo= (Event) => {
 
-        event = Event
+  //use to re-render the entire list of to-dos    
+  const NewTodo= (Event) => {
     
         if (Event.key === "Enter" && Event.target.value != "") {
           
           increment = increment + 1  
-    
 
           let task = Event.target.value
           
@@ -29,64 +27,45 @@ export function CreateInput() {
 
           console.log("the task is:", state)
 
+          //append new task to array of tasks
+          // ArrayOfTasks.push(task);
+          // console.log("the array of tasks is:", ArrayOfTasks)
+          //save array in localstorage
+          // localStorage.setItem('todoArray', JSON.stringify(ArrayOfTasks));
+
+          //unhide the 'All', 'Active' and 'Completed' buttons
           section = document.getElementsByClassName("main")[0]
           footer = document.getElementsByClassName("footer")[0]      
           section.style.display = "block"
           footer.style.display ="block"
+          //clear new task input
           document.querySelector('#writeTodo').value = '';
+          //show number of live todos
           document.querySelector('#todo-items-count').innerHTML = increment;
           
-    
+          //retrieve the tasks array from localstorate
+          // ArrayOfTasks = localStorage.getItem('todoArray');
+          // parse array of tasks so it can be used in js
+          // ArrayOfTasks = JSON.parse(ArrayOfTasks);
+
           function createNewTask() {
-          
-            return ( 
-              <li key={task} >
-                <div className="view">
-                  <input type="checkbox" className="toggle"/>
-                  <label value={task} >{task}</label>
-                  <button className="destroy"></button>
-                </div>
-                <input className="edit" value={task}></input>
-              </li>
-              
-            )
+             return(
+                <li key={task} >
+                  <div className="view">
+                    <input type="checkbox" className="toggle"/>
+                    <label value={task} >{task}</label>
+                    <button className="destroy"></button>
+                  </div>
+                  <input className="edit" value={task}></input>
+                </li>
+             )
           }
 
           let newTask = Web_pilot.createElement(createNewTask);
           Web_pilot.render(newTask, appendHere);
 
-
         }
         }
-
-        // function GetCount(e){
-
-        //   if (e.key === "Enter" && e.target.value != ""){
-
-        //     console.log("we are inside GetCount")
-
-        //     increment = increment + 1
-
-        //     function ItsWorking(){
-        //       return (
-        //         <span className="todo-count">
-        //         <strong>{increment}</strong>{" "}
-        //         {increment === 1 ? " item" : " items"} left
-        //       </span>
-        //       )
-        //     }
-
-        //     console.log(displayNum)
-           
-        //     let showNum = Web_pilot.createElement(ItsWorking);
-        //     Web_pilot.render(showNum, displayNum);
-
-        //   }
-
-
-
-
-     //   }
 
       
         return (
--- web_pilot/web_pilot.jsx
+++ web_pilot/web_pilot.jsx
@@ -259,7 +259,7 @@ function updateHostComponent(fiber) {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber)
   }
-  reconcileChildren(fiber, fiber.props.children)
+  reconcileChildren(fiber, fiber.props.children.flat())
 }
 
 function reconcileChildren(wipFiber, elements) {
