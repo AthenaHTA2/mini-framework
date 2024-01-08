8740d58b812374398c876c99f895cc036900c1d7
HannahEA <hnnh.almu@gmail.com>
2024-01-08 15:58:30 +0000

    toggle item, edit item events working

---
+++ components/creatNewTask.jsx
@@ -0,0 +1,96 @@
+import {Web_pilot} from "../web_pilot/web_pilot.jsx"
+import { task, dataId, listInfo } from "./createInput"
+
+
+/** @jsx Web_pilot.createElement */
+
+export function createNewTask() {
+    function EditTaskItem(e) {
+        //on double click: editing class
+        // let item = document.getElementById(`${e.target.tag}`)
+        // console.log("edit item: I work!", item, e)
+        // item.classList.add('editing')
+        listInfo.map((l, i) => {
+          if (l.id == e.target.tag){
+            let val = e.target.value
+           listInfo.splice(i, 1, {id: l.id, label:val, class: l.class+" editing"})
+          } 
+    })
+        // listInfo = listInfoC
+        console.log("edit listInfo", listInfo, e.target.value)
+        const appendHere = document.getElementsByClassName("main")[0];
+          let newTask = Web_pilot.createElement(createNewTask)
+          Web_pilot.render(newTask, appendHere)
+      }
+    
+      function ToggleTaskItem(e) {
+        // on click checkbox: completed class
+        console.log("toggle task: I work!")
+        listInfo.map((l, i) => {
+          if (l.id == e.target.tag){
+            if (l.flag === "completed") {
+              listInfo.splice(i, 1, {id: l.id, label:l.label, class:l.class.replace("completed", "active"), flag: "active"})
+            } else {
+              console.log("uncompleted")
+              listInfo.splice(i, 1, {id: l.id, label:l.label, class: "completed"+l.class, flag:"completed"})
+            } 
+          } 
+
+        })
+        let listInfoC = listInfo.filter((l) => l.flag === "completed")
+        if (listInfoC.length > 0) {
+          document.getElementsByClassName('clear-completed')[0].style.display = "block"
+        } else {
+          document.getElementsByClassName('clear-completed')[0].style.display = "none"
+        }
+        console.log("toggle list item",e.target.value, listInfo)
+        const appendHere = document.getElementsByClassName("main")[0];
+          let newTask = Web_pilot.createElement(createNewTask)
+          Web_pilot.render(newTask, appendHere)
+     }
+    
+      function DeleteTaskItem(e) {
+        // on hover: delete button
+        console.log("delete task: I work!")
+      } 
+      function StopEditing(e) {
+        if (e.key == "Enter") {
+            // let item = document.getElementById(`${e.target.tag}`)
+            // item.classList.remove("editing")
+            // let child = item.childNodes[0].childNodes[1]
+            // child.innerHTML = e.target.value 
+          listInfo.map((l, i) => {
+
+            if (l.id == e.target.tag){
+              
+              listInfo.splice(i, 1, {id: l.id, label:e.target.value, class: (l.class).replace(" editing", "")})
+            } 
+          })
+          console.log("stop2 listInfo", listInfo)
+          const appendHere = document.getElementsByClassName("main")[0];
+          let newTask = Web_pilot.createElement(createNewTask)
+          Web_pilot.render(newTask, appendHere)
+        }
+      }
+      //array of list information
+      // let id = "li" + dataId.toString(10)  
+
+    
+    return ( 
+        //loop through list info to make the list
+        <ul className="todo-list">
+        {listInfo.map((l) => {
+        return(
+        <li id={l.id} className={l.class} onDblClick={(e)=>{console.log(e)}} >
+            <div className="view">
+                <input tag={l.id} type="checkbox" className="toggle" onChange={(e) => {ToggleTaskItem(e)}} />
+                <label value={l.label} tag={l.id} onDblClick={(e) => {EditTaskItem(e)}}> {l.label} </label>
+                <button onClick={(e) => {DeleteTaskItem(e)}} className="destroy"></button>
+            </div>
+            <input tag={l.id} onKeyDown={(e) => {StopEditing(e)}} className="edit" value={l.label}></input>
+        </li>)
+        
+    })}
+      </ul>      
+    )
+}
--- components/createInput.jsx
+++ components/createInput.jsx
@@ -1,41 +1,39 @@
   import {Web_pilot} from "../web_pilot/web_pilot.jsx"
-  
+  import {createNewTask } from "./creatNewTask.jsx";
+
+  export var task 
+  export var dataId = 0 
+  export var listInfo = []
+
   //=====> function to create a TODO item <========
 
   /** @jsx Web_pilot.createElement */
-export function CreateInput() {
+  export function CreateInput() {
   
       const NewTodo= (event) => {
     
         if (event.key === "Enter") {
-    
-          const appendHere = document.querySelector(".todo-list");
-          let task = event.target.value
-          const [state, setState] = Web_pilot.useState(task);  
+          //append here 
+          const appendHere = document.getElementsByClassName("main")[0];
+         
+          //get task label
+          task = event.target.value
+          console.log("task label", task)
+          // const [state, setState] = Web_pilot.useState(task);  
 
+          // set section and footer to visible
           section = document.getElementsByClassName("main")[0]
           footer = document.getElementsByClassName("footer")[0]
           section.style.display = "block"
           footer.style.display ="block"
-    
-          function createNewTask() {
           
-            return ( 
-              <li key={state} >
-                <div className="view">
-                  <input type="checkbox" className="toggle"/>
-                  <label value={state}>{state}</label>
-                  <button className="destroy"></button>
-                </div>
-                <input className="edit" value={state}></input>
-              </li>
-            )
-          }
-         
+          //append to array of list info and pass aas a prop to create new task
+          let id = "li" + dataId.toString(10) 
+          listInfo.push({id:id, label:task, class: "", flag: "active"})
+      
           let newTask = Web_pilot.createElement(createNewTask)
-    
           Web_pilot.render(newTask, appendHere)
-    
+          dataId++
         }
       };
     
